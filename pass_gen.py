87d1f01af1e0b270ec583b383889a589903d2c72
Erick <erick@macbook-pro.local>
2023-03-04 22:09:41 -0600

    Added input validation

--- pass_gen.py
+++ pass_gen.py
@@ -7,13 +7,20 @@ upper_case="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = "1234567890"
 symbols = "!@#$%&^*\/"
 
-# combine the character types
-use_for = lower_case + upper_case + number + symbols
+try:
+    # combine the character types
+    use_for = lower_case + upper_case + number + symbols
 
-# user arguments as input for character length
-length = int(sys.argv[1])
+    
+    # user arguments as input for character length
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        length = int(sys.argv[1])
+    else:
+        length = 8
 
-# randomly generate the password
-password = "".join(random.sample(use_for, length))
+    # randomly generate the password
+    password = "".join(random.sample(use_for, length))
 
-print(password)
+    print(password)
+except ValueError:
+    print('Error: Invalid argument. Please use an integer.')
